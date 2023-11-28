0ebc9cd77765b1e5b5ba1676c1bbced8fba3326e
Samuel Henao <codinsamueldev@gmail.com>
2023-11-27 20:13:10 -0500

    update class_objects file

--- class_objects.py
+++ class_objects.py
@@ -1,2 +1,18 @@
-class Lola():
-    piel = "harsh"
+#Everything is an object, so classes are the things that compose that object.
+
+#We use Pascal case when naming a class. 
+class Celular(): 
+    marca = "Samsung" 
+    modelo = "S23" 
+    camara = "48MP" 
+    
+#We can create many objects as we want and link them with the attributes set up before. 
+
+celular1 = Celular() 
+celular2 = Celular() 
+celular3 = Celular() 
+celular4 = Celular()
+
+print(type(celular1.modelo)) 
+print(celular1.marca) 
+print(celular1.camara)
