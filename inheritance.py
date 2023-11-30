27921c9bde144c13b2b2cf266355fb751a0c0efb
Samuel Henao <codinsamueldev@gmail.com>
2023-11-30 11:21:14 -0500

    update inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -17,7 +17,7 @@ class Persona:
     def hablar(self):
         return "Hola estoy hablando..."
 
-#Inheritance of Persona class.
+#Inheritance of Persona class. On the other hand, this is an example of basic inheritance.
 class Empleado(Persona):
     def __init__(self, nombre, edad, nacionalidad, trabajo, salario):
         #Specify which elements we want to inherit.
@@ -25,9 +25,24 @@ class Empleado(Persona):
         self.trabajo = trabajo
         self.salario = salario
 
+    def hablar(self):
+        return "No quiero..."
+
+
+#Now, we are creating a hierarchical inheritance because, Empleado and Estudiante are using same parent class called Persona.
+class Estudiante(Persona):
+    def __init__(self, nombre, edad, nacionalidad, notas, semestre):
+        #Specify which elements we want to inherit.
+        super().__init__(nombre, edad, nacionalidad)
+        self.notas = notas
+        self.semestre = semestre
 
 roberto = Empleado("Roberto", "33", "Peruano", "Ladron", 100000)
+ana = Estudiante("Ana", "20", "Boliviana", 4.1, "Segundo")
 
 #We can inherite methods as well.
 print(roberto.hablar())
 print(roberto.trabajo)
+
+print(f'\n{ana.notas}')
+print(ana.semestre)
