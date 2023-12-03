9dc62d16c277e58bec8e79367ed5cb44e3042919
Samuel Henao <codinsamueldev@gmail.com>
2023-12-03 12:44:44 -0500

    update encapsulation.py

--- encapsulation.py
+++ encapsulation.py
@@ -6,7 +6,7 @@ For private attributes, we need to declared methods in order to access the value
 And for protected attributes, neither user and programmer can access the value.
 
 In Python, we can give an attribute the protected security, using two under-scores (self.__nameOfTheVariable).
-
+Nevertheless, since we cannot give those kind of security to attributes natively, we can still access them using ._NameOfClass__AttributeOrMethodName
 """
 
 class Clase:
@@ -14,9 +14,17 @@ class Clase:
         self._priv = "12345678"
         self.__priv = "contraseña"
 
+    def __methodSecured(self):
+        return "Hoy te dedico\nMis mejores pregones..."
+
 
 test = Clase()
 
+""" The following prints will output attribute's values. """
 print(test._priv)
-#This print will output an error.
-print(test.__priv)
+print(test._Clase__priv)
+print(test._Clase__methodSecured())
+
+""" The following prints will output error. """
+#print(test.__priv)
+#print(test.__methodSecured())
