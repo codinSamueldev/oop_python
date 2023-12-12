a9abd618942cb9104ca5a16f6a4ddb94c2bda032
Samuel Henao <codinsamueldev@gmail.com>
2023-12-12 13:23:26 -0500

    further exemplification of LSP principle

--- solid_principles/02_l.py
+++ solid_principles/02_l.py
@@ -33,3 +33,96 @@ class BirdWhoCanFly(Bird):
 class BirdWhoCannotFly(Bird):
     def fly(self):
         return "I cannot fly"
+    
+
+### 2nd example. ###
+
+class KitchenAppliance:
+    def turn_on():
+        pass
+
+
+    def turn_off():
+        pass
+
+
+"""  
+Some Kitchen appliances does not require a temperature feature, 
+so in order to have every single appliance inherit the correct methods,
+we need to create a subclass for appliances that requires set up temperature.
+This is aplicable for everything, some objects may have common attributes but not always has every single attribute.
+"""
+
+class KitchenApplianceWithTemperature(KitchenAppliance):
+    def set_temperature():
+        pass
+
+
+class Toaster(KitchenApplianceWithTemperature):
+    def turn_on():
+        pass
+
+
+    def turn_off():
+        pass
+
+    def set_temperature():
+        pass
+
+
+class Juicer(KitchenAppliance):
+    def turn_on():
+        pass
+
+    def turn_off():
+        pass
+
+
+### 3rd Example. ###
+
+class Animal:
+    def walk():
+        pass
+
+    def run():
+        pass
+
+    def procreate():
+        pass
+
+
+class AnimalsWithTwoLegs(Animal):
+    def fly():
+        pass
+
+class AnimalsWithFourLegs(Animal):
+    def crawl():
+        pass
+
+
+class Bird(AnimalsWithTwoLegs):
+    def walk():
+        pass
+
+    def run():
+        pass
+
+    def procreate():
+        pass
+
+    def fly():
+        pass
+
+
+class Dog(AnimalsWithFourLegs):
+    def walk():
+        pass
+
+    def run():
+        pass
+
+    def procreate():
+        pass
+
+    def crawl():
+        pass
